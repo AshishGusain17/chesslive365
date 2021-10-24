c243954b61b7be7bb6a8c10844db354b58809245
AshishGusain17 <ashishgusain12345@gmail.com>
2021-10-24 13:14:32 +0530

    few css fixes

--- src/components/Followups/Connect.js
+++ src/components/Followups/Connect.js
@@ -6,14 +6,14 @@ export const Connect = () => {
     return (
         <div className={socialStyles.social}>
 
-            <svg
-                height="29"
-                width="25"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 256 256"
-            >
-                <g fill="none">
-                    <a href="https://www.linkedin.com/in/ashish-gusain-257b841a2/">
+            <a href="https://www.linkedin.com/in/ashish-gusain-257b841a2/">
+                <svg
+                    height="26"
+                    width="23"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 256 256"
+                >
+                    <g fill="none">
                         <path
                             d="M0 18.338C0 8.216 8.474 0 18.92 0h218.16C247.53 0 256 8.216 256 18.338v219.327C256 247.79 247.53 256 237.08 256H18.92C8.475 256 0 247.791 0 237.668V18.335z"
                             fill="#069"
@@ -22,16 +22,16 @@ export const Connect = () => {
                             d="M77.796 214.238V98.986H39.488v115.252H77.8zM58.65 83.253c13.356 0 21.671-8.85 21.671-19.91-.25-11.312-8.315-19.915-21.417-19.915-13.111 0-21.674 8.603-21.674 19.914 0 11.06 8.312 19.91 21.169 19.91h.248zM99 214.238h38.305v-64.355c0-3.44.25-6.889 1.262-9.346 2.768-6.885 9.071-14.012 19.656-14.012 13.858 0 19.405 10.568 19.405 26.063v61.65h38.304v-66.082c0-35.399-18.896-51.872-44.099-51.872-20.663 0-29.738 11.549-34.78 19.415h.255V98.99H99.002c.5 10.812-.003 115.252-.003 115.252z"
                             fill="#fff"
                         />
-                    </a>
-                </g>
-            </svg>
+                    </g>
+                </svg>
+            </a>
 
 
 
             <a href="https://github.com/AshishGusain17/">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64" aria-labelledby="title"
-                    height="25"
-                    width="25"
+                    height="23"
+                    width="23"
                     aria-describedby="desc" role="img">
                     <path data-name="layer2"
                         d="M32 0a32.021 32.021 0 0 0-10.1 62.4c1.6.3 2.2-.7 2.2-1.5v-6c-8.9 1.9-10.8-3.8-10.8-3.8-1.5-3.7-3.6-4.7-3.6-4.7-2.9-2 .2-1.9.2-1.9 3.2.2 4.9 3.3 4.9 3.3 2.9 4.9 7.5 3.5 9.3 2.7a6.93 6.93 0 0 1 2-4.3c-7.1-.8-14.6-3.6-14.6-15.8a12.27 12.27 0 0 1 3.3-8.6 11.965 11.965 0 0 1 .3-8.5s2.7-.9 8.8 3.3a30.873 30.873 0 0 1 8-1.1 30.292 30.292 0 0 1 8 1.1c6.1-4.1 8.8-3.3 8.8-3.3a11.965 11.965 0 0 1 .3 8.5 12.1 12.1 0 0 1 3.3 8.6c0 12.3-7.5 15-14.6 15.8a7.746 7.746 0 0 1 2.2 5.9v8.8c0 .9.6 1.8 2.2 1.5A32.021 32.021 0 0 0 32 0z"
@@ -44,16 +44,16 @@ export const Connect = () => {
 
 
             <a href="https://www.chess.com/stats/live/rapid/ashishgusain17">
-                <img style={{ height: "25px", width: "25px" }} src={chessicon} alt="chess.com" ></img>
+                <img style={{ height: "23px", width: "23px" }} src={chessicon} alt="chess.com" ></img>
             </a>
 
 
 
             <a href="https://www.youtube.com/watch?v=LHlgFDLzG00">
                 <svg xmlns="http://www.w3.org/2000/svg"
-                    viewBox="5 5 48 48"
-                    width="35px"
-                    height="35px">
+                    viewBox="0 1 48 48"
+                    width="30px"
+                    height="30px">
                     <path fill="#FF3D00" d="M43.2,33.9c-0.4,2.1-2.1,3.7-4.2,4c-3.3,0.5-8.8,1.1-15,1.1c-6.1,0-11.6-0.6-15-1.1c-2.1-0.3-3.8-1.9-4.2-4C4.4,31.6,4,28.2,4,24c0-4.2,0.4-7.6,0.8-9.9c0.4-2.1,2.1-3.7,4.2-4C12.3,9.6,17.8,9,24,9c6.2,0,11.6,0.6,15,1.1c2.1,0.3,3.8,1.9,4.2,4c0.4,2.3,0.9,5.7,0.9,9.9C44,28.2,43.6,31.6,43.2,33.9z" />
                     <path fill="#FFF" d="M20 31L20 17 32 24z" />
                 </svg>
