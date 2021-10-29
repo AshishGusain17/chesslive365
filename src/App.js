d909a891b16d3b72e54a884d147f05ad4524efc8
AshishGusain17 <ashishgusain12345@gmail.com>
2021-10-29 17:27:07 +0530

    loading page added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
-// import "./App.css";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import About from "./components/About";
 import PositionState from './context/position/PositionState';
 import Alert from "./components/Alerts/Alert";
 import Home from "./components/Home";
@@ -11,12 +9,12 @@ import { Heart } from "./components/Followups/Heart";
 
 export default function App() {
 
-  const [alertObj, setAlert] = useState(null)
+  const [alertObj, setAlert] = useState(null);
   const alertCall = (msg, msgText, timeLimit) => {
     setAlert({ msg: msg, msgText: msgText });
-      setTimeout(() => {
-        setAlert(null);
-      }, timeLimit);
+    setTimeout(() => {
+      setAlert(null);
+    }, timeLimit);
   }
 
   const nullifyAlert = () => {
@@ -25,28 +23,46 @@ export default function App() {
 
 
 
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const fakeRequest = () => {
+      return new Promise(resolve => setTimeout(() => resolve(), 5000));
+    };
+    fakeRequest().then(() => {
+      const el = document.querySelector(".loader-container");
+      if (el) {
+        el.remove();  // removing the spinner element
+        setLoading(false); // showing the app
+      }
+    });
+  })
 
-  return (
-    <>
-      <PositionState>
-        <Router>
 
-          <Switch>
-            <Route exact path="/">
-              <Home alertCall={alertCall} nullifyAlert={nullifyAlert} />
-            </Route>
-            <Route exact path="/about">
-              <About />
-            </Route>
-            <Route path="/live">
-              <Live alertObj={alertObj} alertCall={alertCall} nullifyAlert={nullifyAlert} />
-            </Route>
-          </Switch>
-          <Heart />
-          <Alert alertObj={alertObj} nullifyAlert={nullifyAlert} />
 
-        </Router>
-      </PositionState>
-    </>
-  );
+  // Downloading files to display, till than a fake request
+  if (loading) {
+    return null;
+  }
+  else {
+    return (
+      <>
+        <PositionState>
+          <Router>
+
+            <Switch>
+              <Route exact path="/">
+                <Home alertCall={alertCall} nullifyAlert={nullifyAlert} />
+              </Route>
+              <Route path="/live">
+                <Live alertObj={alertObj} alertCall={alertCall} nullifyAlert={nullifyAlert} />
+              </Route>
+            </Switch>
+            <Heart />
+            <Alert alertObj={alertObj} nullifyAlert={nullifyAlert} />
+
+          </Router>
+        </PositionState>
+      </>
+    );
+  }
 }
